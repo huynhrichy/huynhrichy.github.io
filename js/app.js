2b786bb6f4946bdfc8bcb6dccd9ad1cd18aa60a6
Richard Huynh <richardbaohuynh@gmail.com>
2014-10-07 19:26:13 -0400

    Grid for projects

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+
 $(document).ready(function() {
 	$('#name').hide().fadeIn(1000, function() {
 	});
+
+	var grid = document.querySelector('#grid');
+
+var msnry = new Masonry(grid, {
+	isFitWidth: true,
+	itemSelector: '.project',
+	columnWidth: 500,
+	transitionDuration: 0
+	//gutter: 10
+
+	//isOriginLeft:
+});
+
 });
