833eaa199e434df0cd8566472c500a58ade64917
Richard Huynh <richardbaohuynh@gmail.com>
2014-10-10 10:59:06 -0400

    Links and images for projects and social networks

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
-
 $(document).ready(function() {
 	$('#name').hide().fadeIn(1000, function() {
 	});
-/*
-	var grid = document.querySelector('#grid');
-
-var msnry = new Masonry(grid, {
-	isFitWidth: true,
-	itemSelector: '.project',
-	columnWidth: 500,
-	transitionDuration: 0
-	//gutter: 10
-
-	//isOriginLeft:
-});
-*/
 });
